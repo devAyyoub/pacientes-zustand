23a55d9f143c43667ff14bc789b5e0d256f6a763
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-09 01:26:07 +0200

    filling the patient form when pressing edit a patient

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -2,16 +2,31 @@ import { useForm } from "react-hook-form";
 import Error from "./Error";
 import type { DraftPatient } from "../types";
 import { usePatientStore } from "../store";
+import { useEffect } from "react";
 
 export default function PatientForm() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
     reset
   } = useForm<DraftPatient>();
 
   const addPatient = usePatientStore(state => state.addPatient)
+  const activeId = usePatientStore(state => state.activeId)
+  const patients = usePatientStore(state => state.patients)
+
+  useEffect(() => {
+    if(activeId) {
+        const activePatient = patients.filter(patient => patient.id === activeId)[0]
+        setValue('name', activePatient.name)
+        setValue('caretaker', activePatient.caretaker)
+        setValue('date', activePatient.date)
+        setValue('email', activePatient.email)
+        setValue('symptoms', activePatient.symptoms)
+    }
+  }, [activeId])
 
   const registerPatient = (data: DraftPatient) => {
     addPatient(data)

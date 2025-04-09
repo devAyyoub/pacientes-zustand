c50efa9cae65c0e992e761d3f997890c1c76bf1e
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-09 02:06:26 +0200

    setting in localstorage the zustand store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 import { DraftPatient, Patient } from "./types";
 import { v4 as uuidv4 } from "uuid";
 
@@ -17,34 +17,41 @@ const createPatient = (patient: DraftPatient): Patient => {
 };
 
 export const usePatientStore = create<PatientState>()(
-  devtools((set) => ({
-    patients: [],
-    activeId: "",
-    addPatient: (data) => {
-      const newPatient = createPatient(data);
-      set((state) => ({
-        patients: [...state.patients, newPatient],
-      }));
-    },
-    deletePatient: (id) => {
-      set((state) => ({
-        patients: state.patients.filter((patient) => patient.id !== id),
-      }));
-    },
-    getPatientById(id) {
-      set(() => ({
-        activeId: id,
-      }));
-    },
-    updatePatient(data) {
-      set((state) => ({
-        patients: state.patients.map((patient) =>
-          patient.id === state.activeId
-            ? { id: state.activeId, ...data }
-            : patient
-        ),
+  devtools(
+    persist(
+      (set) => ({
+        patients: [],
         activeId: "",
-      }));
-    },
-  }))
+        addPatient: (data) => {
+          const newPatient = createPatient(data);
+          set((state) => ({
+            patients: [...state.patients, newPatient],
+          }));
+        },
+        deletePatient: (id) => {
+          set((state) => ({
+            patients: state.patients.filter((patient) => patient.id !== id),
+          }));
+        },
+        getPatientById(id) {
+          set(() => ({
+            activeId: id,
+          }));
+        },
+        updatePatient(data) {
+          set((state) => ({
+            patients: state.patients.map((patient) =>
+              patient.id === state.activeId
+                ? { id: state.activeId, ...data }
+                : patient
+            ),
+            activeId: "",
+          }));
+        },
+      }),
+      {
+        name: "patient-storage",
+      }
+    )
+  )
 );
